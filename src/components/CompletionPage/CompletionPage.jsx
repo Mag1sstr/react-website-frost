8f19848c729b9da2d37fcb9232de7cce196b54d5
Mag1sstr <danilkarachev234@mail.ru>
2024-10-28 21:43:46 +0500

    fix bug

--- src/components/CompletionPage/CompletionPage.jsx
+++ src/components/CompletionPage/CompletionPage.jsx
@@ -1,23 +1,26 @@
-import "./CompletionPage.css";
-import check from "./../../images/check.svg";
+import "./CompletionPage.css"
+import check from "./../../images/check.svg"
+import { useContext } from "react"
+import { AuthContext } from "../../contexts/Auth/AuthContextProvider"
 export default function CompletionPage() {
-  return (
-    <div className="conteiner">
-      <div className="comletionpage">
-        <div className="comletionpage__inner">
-          <h1 className="comletionpage__title">Заказ успешно создан</h1>
-          <div className="comletionpage__row">
-            <img src={check} alt="" />
-            <p className="comletionpage__text">
-              Заказ №100001 был создан. Вы можете просмотреть список всех ваших
-              заказов в личном кабинете.
-            </p>
-            <button className="comletionpage__btn">
-              Перейти в личный кабинет
-            </button>
-          </div>
-        </div>
+   const orderNumber = useContext(AuthContext)
+   return (
+      <div className="conteiner">
+         <div className="comletionpage">
+            <div className="comletionpage__inner">
+               <h1 className="comletionpage__title">Заказ успешно создан</h1>
+               <div className="comletionpage__row">
+                  <img src={check} alt="" />
+                  <p className="comletionpage__text">
+                     Заказ №{orderNumber.orderNumber} был создан. Вы можете
+                     просмотреть список всех ваших заказов в личном кабинете.
+                  </p>
+                  <button className="comletionpage__btn">
+                     Перейти в личный кабинет
+                  </button>
+               </div>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   )
 }
